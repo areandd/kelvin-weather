9b05747e4d04fd70b10fedb4c698c003450462a0
Isaiah Desta <areandd@protonmail.com>
2022-08-02 14:51:29 +0000

    kelvin to fahrenheit calculator

---
+++ app.js
@@ -0,0 +1,12 @@
+const kelvin = 400;
+const celsius = kelvin - 273;
+
+let fahrenheit = celsius * (9/5) + 32;
+
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
+
+
+// console.log(celsius);
+// console.log(fahrenheit);
